1783c25d5124dad226a5fed08d949ec0de5583a4
dawidpos <58737241+dawidpos@users.noreply.github.com>
2023-01-11 10:11:34 +0100

    1 commit

---
+++ SimpleProjectManager.Module/BusinessObjects/Klient.cs
@@ -0,0 +1,102 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using DevExpress.Xpo.Metadata;
+using System;
+using System.Collections;
+
+namespace SimpleProjectManager.Module.BusinessObjects.Klient
+{
+
+    [NavigationItem("Klient")]
+    public class Klient : BaseObject
+    {
+        public Klient(Session session) : base(session) { }
+
+        // edycja pól
+        string symbol;
+        public string Symbol
+        {
+            get { return symbol; }
+            set { SetPropertyValue(nameof(Symbol), ref symbol, value); }
+        }
+        string vatNumber;
+        public string VatNumber
+        {
+            get { return vatNumber; }
+            set { SetPropertyValue(nameof(VatNumber), ref vatNumber, value); }
+        }
+        string klientName;
+        public string KlientName
+        {
+            get { return klientName; }
+            set { SetPropertyValue(nameof(KlientName), ref klientName, value); }
+        }
+        string street;
+        public string Street
+        {
+            get { return street; }
+            set { SetPropertyValue(nameof(Street), ref street, value); }
+        }
+
+
+        string city;
+        public string City
+        {
+            get { return city; }
+            set { SetPropertyValue(nameof(City), ref city, value); }
+        }
+
+
+        string postalCode;
+        public string PostalCode
+        {
+            get { return postalCode; }
+            set { SetPropertyValue(nameof(PostalCode), ref postalCode, value); }
+        }
+
+        string email;
+        public string Email
+        {
+            get { return email; }
+            set { SetPropertyValue(nameof(Email), ref email, value); }
+        }
+        string regon;
+        public string Regon
+        {
+            get { return regon; }
+            set { SetPropertyValue(nameof(Regon), ref regon, value); }
+
+        }
+
+        DateOnly dateOfCreation;
+        public DateOnly DateOfCreation
+        {
+            get { return dateOfCreation; }
+            set { SetPropertyValue(nameof(DateOfCreation), ref dateOfCreation, value); }
+
+        }
+        DateOnly dateOfCommencement;
+        public DateOnly DateOfCommencement
+        {
+            get { return dateOfCommencement; }
+            set { SetPropertyValue(nameof(DateOfCommencement), ref dateOfCommencement, value); }
+        }
+        DateOnly dateOfEntry;
+        public DateOnly DateOfEntry 
+        {
+            get { return dateOfEntry; }
+            set { SetPropertyValue(nameof(DateOfEntry), ref dateOfEntry, value); }
+        }
+
+        DateOnly dateOfChangeOccur;
+        public DateOnly DateOfChangeOccur 
+        {
+            get { return dateOfChangeOccur; }
+            set { SetPropertyValue(nameof(DateOfChangeOccur), ref dateOfChangeOccur, value); }
+        }
+        
+     
+
+    }
+}
---
+++ SimpleProjectManager.Module/Controllers/KlientController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using SimpleProjectManager.Module.BusinessObjects.Klient;
+namespace SimpleProjectManager.Module.Controllers
+{
+    public class KlientController : ViewController
+    {
+        public KlientController()
+       {
+
+       
+
+
+            TargetObjectType = typeof(Klient);
+            TargetViewType = ViewType.Any;
+            SimpleAction getData = new SimpleAction(
+                this, "Get data from GUS",
+                DevExpress.Persistent.Base.PredefinedCategory.RecordEdit)
+            {
+             
+            };
+
+           
+
+    
+        }
+       
+     
+
+    }
+}
